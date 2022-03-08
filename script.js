c3c7b8b678d693190e9658041d41d40a5605f41e
Ahmet Zorlutuna <ahmettayyip3439@gmail.com>
2022-03-08 22:30:01 +0300

    değişken tipleri

--- script.js
+++ script.js
@@ -9,16 +9,85 @@
 
 //Değişkenler
 
-var age;
-age = 5;
-console.log(age);
+// var age;
+// age = 5;
+// console.log(age);
 
-var fullname;
-fullname = 'ahmetzorlutuna';
-console.log(fullname);
+// var fullname;
+// fullname = 'ahmetzorlutuna';
+// console.log(fullname);
 
-fullname = 'ahmetzorlutuna23';
-console.log(fullname);
+// fullname = 'ahmetzorlutuna23';
+// console.log(fullname);
+
+// const email = 'deneme@gmail.com';
+// console.log(email);
 
 //Değişken tanımlama kuralları
+
+
+//Değişken Tipleri
+
+//Primitive Types
+
+    //String
+    let firstName = 'Çınar';
+
+    //Number
+    let age = 20;
+    let money = 100.5;
+
+    //Boolean
+    let isActive = false;
+
+    //null
+    let job = null;
+
+    //undefined
+    let car;
+
+
+//Reference Types - Objects
+
+    //Array
+    let name = ['Ali','Ahmet','Mehmet'];
+
+    //Object
+    let adress = {
+        city: 'Kocaeli',
+        country: 'Türkiye'
+    }
+
+    //Function
+
+    var calculateAge = function(a,b){
+        return a+b;
+    };
+
+    console.log(calculateAge(1,2));
+
+    console.log(typeof calculateAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
