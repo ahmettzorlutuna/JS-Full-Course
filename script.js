d5c6f1266853b38938fb6fab3966af83c7e883ab
Ahmet Zorlutuna <ahmettayyip3439@gmail.com>
2022-03-08 17:19:04 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// console.log("Merhaba");
+// console.log(1234);
+// console.log(true);
+// console.log([1,2,3,4]);
+// console.error("hata oluştu");
+// console.warn("Bir uyarı oluştu");
+
+// console.clear()
+
+//Değişkenler
+
+var age;
+age = 5;
+console.log(age);
+
+var fullname;
+fullname = 'ahmetzorlutuna';
+console.log(fullname);
+
+fullname = 'ahmetzorlutuna2';
+console.log(fullname);
+
+//Değişken tanımlama kuralları
+
