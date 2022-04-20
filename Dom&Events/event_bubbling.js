3bc1e9658f5ff4fcb96fe63afeb5fb41ed74cfa4
Ahmet Zorlutuna <ahmettayyip3439@gmail.com>
2022-04-20 06:03:40 +0300

    goin

--- Dom&Events/event_bubbling.js
+++ Dom&Events/event_bubbling.js
@@ -7,52 +7,52 @@ const container = document.querySelector('.container');
 
 //*** Event Bubbling ***
 
-// form.addEventListener('click',function(e){
-//     console.log('form');
-//     e.stopPropagation();
-// });
+form.addEventListener('click',function(e){
+    console.log('form');
+    e.stopPropagation();
+});
 
-// cardBody.addEventListener('click',function(e){
-//     console.log('cardBody');
-//     e.stopPropagation();
-// });
+cardBody.addEventListener('click',function(e){
+    console.log('cardBody');
+    e.stopPropagation();
+});
 
-// card.addEventListener('click',function(e){
-//     console.log('card');
-//     e.stopPropagation();
-// });
+card.addEventListener('click',function(e){
+    console.log('card');
+    e.stopPropagation();
+});
 
-// container.addEventListener('click',function(e){
-//     console.log('container');
-//     e.stopPropagation();
-// });
+container.addEventListener('click',function(e){
+    console.log('container');
+    e.stopPropagation();
+});
 
 //*** Event capturing ***
 
-// form.addEventListener('click',function(e){
-//     console.log('form');
-// },true);
+form.addEventListener('click',function(e){
+    console.log('form');
+},true);
 
-// cardBody.addEventListener('click',function(e){
-//     console.log('cardBody');
-// },true);
+cardBody.addEventListener('click',function(e){
+    console.log('cardBody');
+},true);
 
-// card.addEventListener('click',function(e){
-//     console.log('card');
-// },true);
+card.addEventListener('click',function(e){
+    console.log('card');
+},true);
 
-// container.addEventListener('click',function(e){
-//     console.log('container');
-// },true);
+container.addEventListener('click',function(e){
+    console.log('container');
+},true);
 
 const deleteItems = document.querySelectorAll('.fa-times');
 
 
-// deleteItems.forEach(function(item){
-//     item.addEventListener('click',function(e){
-//         console.log(e.target);
-//     });
-// });
+deleteItems.forEach(function(item){
+    item.addEventListener('click',function(e){
+        console.log(e.target);
+    });
+});
 
 const ul = document.querySelector('ul');
 
