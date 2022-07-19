a1e18759f97345fb5bdb8769b0bc37478424b0c3
Ahmet <ahmettayyip3439@gmail.com>
2022-07-19 23:37:22 +0300

    async

--- APPs/Music Player APP/music.js
+++ APPs/Music Player APP/music.js
@@ -15,5 +15,6 @@ const MusicList = [
     new Music("Bu da Geçer mi Sevgilim", "Yalın","2.jpeg","2.mp3"),
     new Music("Boşver", "Nilüfer","1.jpeg","1.mp3"),
     new Music("Aramızda Uçurumlar", "Suat Suna","3.jpeg","3.mp3"),
-    new Music("Escobar","Killa Hakan","4.jpg","4.mp3")
+    new Music("Escobar","Killa Hakan","4.jpg","4.mp3"),
+    new Music("Tek Şans","Killa Hakan","5.jpg","5.mp3")
 ]
