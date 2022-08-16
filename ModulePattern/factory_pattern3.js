df8b29527598f189c6ca7b7f926560ab3d7bcf59
Ahmet <ahmettayyip3439@gmail.com>
2022-08-16 22:52:48 +0300

    pattern

--- ModulePattern/factory_pattern3.js
+++ ModulePattern/factory_pattern3.js
@@ -1,17 +1,41 @@
-class TailFactory{
-    constructor(props){
-        this.tailLenght = props.tailLenght;
+function MotherBoard(name) {
+    this.name = name
+    this.type = 'Hardware'
+    this.price = '$4500'
+}
+
+function GraphicCard(name) {
+    this.name = name
+    this.type = 'Software'
+    this.price = '$2200'
+}
+
+function ComputerFactory() {
+    this.create = (name, type) => {
+        switch (type) {
+            case 1:
+                return new MotherBoard(name)
+                break;
+            case 2:
+                return new GraphicCard(name)
+                break;
+        }
     }
 }
 
-class TorsoFactory{
-    constructor(props){
-        this.tailLenght = props.color;
-    }
+function showPrice(){
+    console.log(this.name + ' price is ' + this.price)
 }
 
-class HeadFactory{
-    constructor(props){
-        this.tailLenght = props.snoutLength;
-    }
-}
+const computerFactory = new ComputerFactory()
+const parts = []
+parts.push(computerFactory.create('MSI',1))
+parts.push(computerFactory.create('Nvidia',2))
+
+parts.forEach(part=>{
+    console.log(part.name,part.type);
+    showPrice.call(part);
+})
+
+
+
